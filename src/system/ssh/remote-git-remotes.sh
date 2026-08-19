be0441753c81aa3c570f30f40516302fc4c99948
nano-props <nano@nano.properties>
2026-08-19 09:15:03 +0800

    fix: preserve multi-url remote metadata records

--- src/system/ssh/remote-git-remotes.sh
+++ src/system/ssh/remote-git-remotes.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 REPO_PATH=$1
 
 while IFS= read -r REMOTE; do
-  FETCH_URL=$(git -C "$REPO_PATH" remote get-url --all -- "$REMOTE")
-  PUSH_URL=$(git -C "$REPO_PATH" remote get-url --push --all -- "$REMOTE")
-  printf '%s\t%s\t%s\n' "$REMOTE" "$FETCH_URL" "$PUSH_URL"
+  mapfile -t FETCH_URLS < <(git -C "$REPO_PATH" remote get-url --all -- "$REMOTE")
+  mapfile -t PUSH_URLS < <(git -C "$REPO_PATH" remote get-url --push --all -- "$REMOTE")
+  ((${#FETCH_URLS[@]} > 0 && ${#PUSH_URLS[@]} > 0))
+  printf '%s\t%s\t%s\n' "$REMOTE" "${FETCH_URLS[-1]}" "${PUSH_URLS[-1]}"
 done < <(git -C "$REPO_PATH" remote)
